bb60e1cfa7c17a736d2dc44f685a66a5e1e96e9b
anant10 <anantshanbhag93@gmail.com>
2020-10-11 14:58:58 -0400

    initial commit

---
+++ src/test/java/com/sb/basics/sbstarter/SbStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.basics.sbstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
